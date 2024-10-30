2de11403565c1217334e40ca6f9bc867eec0c5e2
HardikAcumen <hprajapati@chuckads.com>
2024-10-30 14:10:00 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div><img className='w-14 h-14 pb-2' src="../../public/logo.png" alt="" /></div>
+  )
+}
+
+export default Logo
