1a25ec19d55ddb95c796d54039af4a52a4b1849a
Hardik7843 <hardikprajapati7843@gmail.com>
2024-10-31 01:20:02 +0530

    hehe

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Logo({width = '100px'}) {
   return (
-    <div><img className='w-14 h-14 pb-2' src="../../public/logo.png" alt="" /></div>
+    // <div><img className='w-14 h-14 pb-2' src="../../public/logo.png" alt="" /></div>
+    <>
+      Logo
+    </>
   )
 }
 
